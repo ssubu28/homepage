f9b21f7eebebac26300a58bae3dc3a2fbe59aee5
Subhashini Sundaresan <ssubu28@gmail.com>
2022-01-23 12:12:03 -0500

    Bash script pages complete.Link-img pending.

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
+cat templates/top.html content/resume.html templates/bottom.html > docs/resume.html
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
+

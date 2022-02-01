e72ca20e9ed6b05ac00f9a6d9c2b456621a0c646
Subhashini Sundaresan <ssubu28@gmail.com>
2022-01-31 21:58:42 -0500

    hw3 modified

--- build.py
+++ build.py
@@ -1,29 +1,17 @@
-# Function to build a single page.
+# Python script to generate a static site
+
+# Function to build a page - replace content and title
 def page_builder(file_content, page_title):
     template = open('./templates/base.html').read()
-    # Title tag replacement
-    nav_page = template.replace('{{title}}', page_title)
+    # Titletag replacement
+    titletag_page = template.replace('{{title}}', page_title)
     # Content replacement
-    content_page = nav_page.replace('{{content}}', file_content)
-    
-    
-    """ 
-    if page_title == 'Tech Projects':
-        full_page = nav_page.replace('{{Active Tech Blog}}', 'active')
-    elif page_title == 'About':
-        full_page = nav_page.replace('{{Active About}}', 'active')
-    elif page_title == 'Photography':
-        full_page = nav_page.replace('{{Active Blog}}', 'active')
-    elif page_title == 'Resume':
-        full_page = nav_page.replace('{{Active Resume}}', 'active')
-    else:
-        full_page = nav_page  
-    """
-    
-    return content_page      # should be full_page if using above if-else block
+    content_page = titletag_page.replace('{{content}}', file_content)
+    return content_page     
 
 
 
+# Activating navigation links 
 def navlink_activator(replacement_text, base_page):
     final_page=base_page.replace(replacement_text, 'active')
     return final_page
@@ -31,62 +19,63 @@ def navlink_activator(replacement_text, base_page):
 
 
 # Function to check which page to build
-def page_identifier(page_title, page_filename, page_output):
-    
-    # *** WILL need IF-ELSE HERE! CHECK TITLE and then proceed with other code.  *** 
+def page_identifier(page_title, page_filename, page_output, page_navigate):
 
     if page_title == 'Tech Projects':
         index_file_read = open(page_filename).read()
         index_base_page = page_builder(index_file_read, page_title)
-        index_final = navlink_activator('{{Active Tech Blog}}', index_base_page)
+        index_final = navlink_activator(page_navigate, index_base_page)
         open(page_output, 'w+').write(index_final)
 
     elif page_title == 'About':
         about_file_read = open(page_filename).read()
         about_base_page = page_builder(about_file_read, page_title)
-        about_final = navlink_activator('{{Active About}}', about_base_page)
+        about_final = navlink_activator(page_navigate, about_base_page)
         open(page_output, 'w+').write(about_final)
 
     elif page_title == 'Photography':
         blog_file_read = open(page_filename).read()
         blog_base_page = page_builder(blog_file_read, page_title)
-        blog_final = navlink_activator('{{Active Blog}}', blog_base_page)
+        blog_final = navlink_activator(page_navigate, blog_base_page)
         open(page_output, 'w+').write(blog_final)
 
     elif page_title == 'Resume':
         resume_file_read = open(page_filename).read()
         resume_base_page = page_builder(resume_file_read, page_title)
-        resume_final = navlink_activator('{{Active Resume}}', resume_base_page)
+        resume_final = navlink_activator(page_navigate, resume_base_page)
         open(page_output, 'w+').write(resume_final)
 
     else:
         print('ERROR! - Invalid Page.')
 
 
-# Main function
+
 def main():
-    # Python script to generate a static page
 
     pages = [
         {
             'title': 'Tech Projects',
             'filename': './content/index.html',
-            'output': './docs/index.html'
+            'output': './docs/index.html',
+            'navigate': '{{Active Tech Blog}}'
         },
         {
             'title': 'About',
             'filename': './content/about.html',
-            'output': './docs/about.html'
+            'output': './docs/about.html',
+            'navigate': '{{Active About}}'
         },
         {
             'title': 'Photography',
             'filename': './content/blog.html',
-            'output': './docs/blog.html'
+            'output': './docs/blog.html',
+            'navigate': '{{Active Blog}}'
         },
         {
             'title': 'Resume',
             'filename': './content/resume.html',
-            'output': './docs/resume.html'
+            'output': './docs/resume.html',
+            'navigate': '{{Active Resume}}'
         }
     ]
 
@@ -94,7 +83,8 @@ def main():
         page_title = page['title']
         page_filename = page['filename']
         page_output = page['output']
-        page_identifier(page_title, page_filename, page_output)
+        page_navigate = page['navigate']
+        page_identifier(page_title, page_filename, page_output, page_navigate)
 
     
 

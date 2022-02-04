6867bb89884789065a16aebb0af4a559a1fd1532
Subhashini Sundaresan <ssubu28@gmail.com>
2022-02-04 16:04:25 -0500

    ph 2.1

--- build.py
+++ build.py
@@ -1,5 +1,8 @@
 # Python script to generate a static site
 
+import glob
+from os import path
+
 # Function to build a page - replace content and title
 def page_builder(file_content, page_title):
     template = open('./templates/base.html').read()
@@ -10,12 +13,10 @@ def page_builder(file_content, page_title):
     return content_page     
 
 
-
 # Activating navigation links 
 def navlink_activator(replacement_text, base_page):
     final_page=base_page.replace(replacement_text, 'active')
     return final_page
-
 
 
 # Function to check which page to build. Refactored.
@@ -26,9 +27,61 @@ def page_identifier(page_title, page_filename, page_output, page_navigate):
     open(page_output, 'w+').write(write_page)
 
 
+def list_generate():
+    pages = []
+
+    # ** For index will have to check and perform a replace or something.  - DONE using if statement
+    # Photography title to be replaced by blog. 
+    # Navigation correction  - DONE
+    
+    all_html_files = glob.glob("content/*.html")
+
+    for file in all_html_files:
+        file_path = file
+        file_name = path.basename(file_path)
+        name_only, file_extension = path.splitext(file_name)
+        output_path = './docs/' + file_name
+          
+        if name_only == 'index':
+            name_only = 'Tech blog'
+
+        # elif name_only == 'blog':
+        #     name_only = 'Photography'
+
+        pages.append({
+            'title': str(name_only.capitalize()),
+            'filename': file_path,
+            'output': output_path,
+            'navigate': '{{Active ' + str(name_only.title()) + '}}'
+        })
+
+    return pages
+
+
+
 def main():
 
-    pages = [
+    pages = list_generate()
+
+    for page in pages:
+        page_title = page['title']
+        page_filename = page['filename']
+        page_output = page['output']
+        page_navigate = page['navigate']
+        page_identifier(page_title, page_filename, page_output, page_navigate)
+
+    
+
+if __name__ == "__main__":
+    main()
+    
+
+
+
+
+
+""" 
+    pages_old = [
         {
             'title': 'Tech Projects',
             'filename': './content/index.html',
@@ -55,14 +108,6 @@ def main():
         }
     ]
 
-    for page in pages:
-        page_title = page['title']
-        page_filename = page['filename']
-        page_output = page['output']
-        page_navigate = page['navigate']
-        page_identifier(page_title, page_filename, page_output, page_navigate)
-
-    
-
-if __name__ == "__main__":
-    main()
+    print("\n\n")
+    for p_old in pages_old:
+        print(p_old) """
